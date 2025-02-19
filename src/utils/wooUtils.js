c888029b96b96bfa689e66b8e29cf65525e9ecf6
ashishmangla0 <ashishmangla1991@gmail.com>
2025-02-19 10:18:40 +0530

    add new code

--- src/utils/wooUtils.js
+++ src/utils/wooUtils.js
@@ -1,14 +1,25 @@
-import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
 const WooCommerce = new WooCommerceRestApi({
-  url: 'https://shop.aavyamart.com/', // Your store URL
-  consumerKey: 'ck_5ff5befe5afa03bd12ddf1f4445129da3d9e8ab0', // Your consumer key
-  consumerSecret: 'cs_ed12e8d833a2f29cc84aacd1f6cbbaa6c7ef3a0a', // Your consumer secret
-  version: 'wc/v3', // WooCommerce API version
+  url: "https://shop.aavyamart.com/", // Your store URL
+  consumerKey: "ck_5ff5befe5afa03bd12ddf1f4445129da3d9e8ab0", // Your consumer key
+  consumerSecret: "cs_ed12e8d833a2f29cc84aacd1f6cbbaa6c7ef3a0a", // Your consumer secret
+  version: "wc/v3", // WooCommerce API version
 });
 
 // Fetch products with pagination
 export const fetchProducts = async (page, perPage) => {
-    console.log(page);
-    console.log(perPage);
-}
+  console.log(page);
+  console.log(perPage);
+
+  try {
+    const response = await WooCommerce.get("products", {
+      page,
+      per_page: perPage,
+    });
+
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
